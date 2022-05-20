594fa53ce850161a9513af79cfd048323871a665
sanjaybaskaran01 <sanjaybaskaran01@gmail.com>
2022-05-20 15:22:03 +0530

    feat: BitBucket CI/CD Pipeline

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /root/koneksys
+
+git reset --hard
+
+git pull origin development
+
+yarn build
+
+pm2 describe koneksys-backend > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start ./pm2-config.json
+else
+  pm2 restart koneksys-backend
+fi;
